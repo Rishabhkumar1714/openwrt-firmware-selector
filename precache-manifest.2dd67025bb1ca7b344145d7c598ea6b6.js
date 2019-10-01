562d19ad570fb4c506175a3bd1ade43fb2358fe8
sudhanshu16 <me@sudhanshug.com>
2019-10-01 15:09:55 +0000

    Deploying to gh-pages from master b466f5689f90af6606f190dcf39f33c89794c7a8

---
+++ precache-manifest.2dd67025bb1ca7b344145d7c598ea6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b75fb4130b28cb393bf1d7ed98880b9",
+    "url": "/openwrt-firmware-selector/index.html"
+  },
+  {
+    "revision": "3a8e5daa9d1f4b0051cc",
+    "url": "/openwrt-firmware-selector/static/css/main.97bcca66.chunk.css"
+  },
+  {
+    "revision": "2b36c73330457d9fbc73",
+    "url": "/openwrt-firmware-selector/static/js/2.41faef2d.chunk.js"
+  },
+  {
+    "revision": "3a8e5daa9d1f4b0051cc",
+    "url": "/openwrt-firmware-selector/static/js/main.33ae0e8d.chunk.js"
+  },
+  {
+    "revision": "f9874eb7040cfcfd9461",
+    "url": "/openwrt-firmware-selector/static/js/runtime~main.91706d78.js"
+  }
+]);
